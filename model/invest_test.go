522816809646678572be7186c905e150732c31ab
Matt Warburton <matthew.warburton@landmark.co.uk>
2019-02-08 18:17:01 +0000

    tests created for instructions calculations

--- model/invest_test.go
+++ model/invest_test.go
@@ -1,7 +1,10 @@
 package model_test
 
 import (
+	"AdAlpha/exchange_rate"
 	"AdAlpha/model"
+	"github.com/jarcoal/httpmock"
+	"os"
 	"testing"
 )
 
@@ -37,5 +40,44 @@ func TestInvest_IsValidReturnsTrueWhenAllFieldsValid(t *testing.T) {
 	b := model.Invest{1, "test01", "GBP", 5.55}
 	if b.IsValid() == false {
 		t.Errorf("Expected response %t. got %t\n", true, false)
+	}
+}
+
+func TestInvest_CalculateInvestIsSuccessfulWhenCurrencyCodesDiffer(t *testing.T) {
+	SetTestingEnvVariables()
+	i := model.Invest{3, "GB00BQ1YHQ70", "USD", 50}
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder("GET", exchange_rate.GenerateUrl("USD", os.Getenv("BASE_CC")),
+		httpmock.NewStringResponder(200, `{"base": "GBP", "date": "2019-02-07", "rates":{"USD":2}}`))
+	err := model.CalculateInvest(5, i)
+	if err != nil {
+		t.Error("Expected no errors")
+	}
+
+	units, valid := correctNumberOfUnitsInPortfolioTable(3, "GB00BQ1YHQ70", 25, 5)
+	if !valid {
+		t.Errorf("Wrong number of units in investor portfolio, expected 25, got %v", units)
+	}
+}
+
+func TestInvest_CalculateInvestIsSuccessfulWhenCurrencyCodesMatch(t *testing.T) {
+	SetTestingEnvVariables()
+	i := model.Invest{3, "GB00BQ1YHQ70", "GBP", 50}
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder("GET", exchange_rate.GenerateUrl("USD", os.Getenv("BASE_CC")),
+		httpmock.NewStringResponder(200, `{"base": "GBP", "date": "2019-02-07", "rates":{"USD":2}}`))
+	err := model.CalculateInvest(5, i)
+	if err != nil {
+		t.Error("Expected no errors")
+	}
+
+	units, valid := correctNumberOfUnitsInPortfolioTable(3, "GB00BQ1YHQ70", 15, 5)
+
+	if !valid {
+		t.Errorf("Wrong number of units in investor portfolio, expected 15, got %v", units)
 	}
 }
--- model/utils_test.go
+++ model/utils_test.go
@@ -2,8 +2,40 @@ package model_test
 
 import (
 	"AdAlpha/model"
+	"database/sql"
+	"fmt"
+	"os"
 	"testing"
 )
+
+func SetTestingEnvVariables() {
+
+	err := os.Setenv("DB_NAME", "test")
+	err = os.Setenv("DB_USERNAME", "test")
+	err = os.Setenv("DB_PASSWORD", "test")
+	err = os.Setenv("DB_PORT", "5001")
+	err = os.Setenv("BASE_CC", "GBP")
+
+	if err != nil {
+		fmt.Println("Issue setting test env variables")
+	}
+}
+
+func correctNumberOfUnitsInPortfolioTable(id int, isin string, expected float64, resetVal float64) (float64, bool) {
+	var units float64
+	dbCon := model.GetDbConnection()
+	err := dbCon.Pg.QueryRow("select units from portfolio where investor_id=$1 and isin=$2", id, isin).Scan(&units)
+	if err != nil && err != sql.ErrNoRows {
+		fmt.Println("error checking units in portfolio table")
+	}
+
+	_, err = dbCon.Pg.Exec("update portfolio set units =$3 where investor_id=$1 and isin=$2", id, isin, resetVal)
+	if err != nil && err != sql.ErrNoRows {
+		fmt.Println("error resetting DB data")
+	}
+	return units, units == expected
+
+}
 
 func TestValidCurrencyCodeWhenCodeIsGBP(t *testing.T) {
 	valid := model.ValidCurrencyCode("GBP")
@@ -58,5 +90,28 @@ func TestContainsWhenArrayDoesNotContainStringReturnsFalse(t *testing.T) {
 	contains := model.Contains([]string{"1", "2"}, "3")
 	if contains != false {
 		t.Errorf("Expected response %t. got %t\n", true, false)
+	}
+}
+
+func TestContainsReturnsTrueWhenArrayContainsVal(t *testing.T) {
+	arr := []string{"cat", "dog"}
+	contains := model.Contains(arr, "cat")
+	if !contains {
+		t.Error("Expected contains to be true")
+	}
+}
+
+func TestContainsReturnsFalseWhenArrayDoesNotContainVal(t *testing.T) {
+	arr := []string{"cat", "dog"}
+	contains := model.Contains(arr, "frog")
+	if contains {
+		t.Error("Expected contains to be false")
+	}
+}
+
+func TestCalculateUnitsReturnsCorrectValue(t *testing.T) {
+	res := model.CalculateUnits(5, 2, 10)
+	if res != 1 {
+		t.Errorf("Calculation wrong, expected 1 and received %b\n", res)
 	}
 }
